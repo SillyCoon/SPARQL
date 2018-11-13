4898a13930b0ca416dce36a0d0aade8c50326f1a
Aleksey Ivanov <ahtunget@gmail.com>
2018-11-13 10:35:21 +0300

    Complete

---
+++ query.py
@@ -0,0 +1,28 @@
+from SPARQLWrapper import SPARQLWrapper, XML, RDF, JSON
+import json
+
+filmUri = "Q72962"
+
+sparql = SPARQLWrapper("http://query.wikidata.org/sparql")
+sparql.setQuery("""
+SELECT DISTINCT ?film ?filmLabel ?myear WHERE {
+  # Находим год выбранного фильма
+  wd:%s wdt:P577 ?mydate.
+  BIND (str(YEAR(?mydate)) as ?myear).
+  
+  # Находим год всех фильмов
+  ?film wdt:P31 wd:Q11424.
+  ?film wdt:P577 ?date.
+  
+  # Выбираем наш фильм и фильмы с тем же годом выхода
+  FILTER(YEAR(?date) = YEAR(?mydate))
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+""" % filmUri);
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+with open("response.json", 'w') as file:
+    file.write(json.dumps(results["results"]["bindings"], indent=4))
+
+
